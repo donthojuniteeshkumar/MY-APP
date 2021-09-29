311550561c460bd2e792b2edead20cdeb4a49144
niteesh <niteesh369@outlook.com>
2021-09-29 14:26:08 +0530

    resume3125

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import User from "./components/user"; 
+
+
+function App() {
+  return (
+    <div>
+       <h1>Users</h1>
+       <User name="Niteesh" description="FSWD" />
+       <User name="Rajesh" description="FSWD" />
+       <User name="Rishi" description="FSWD" />
+       <User name="Jathin" description="FSWD" />
+    </div>
+  );
+}
+
+export default App;
